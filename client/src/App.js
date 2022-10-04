e6fb8bce17127374da2b2219d7ca6801117e5f83
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-10-04 10:32:03 -0700

    react-router-dom installed, link header and footer components, link Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+// import Login from './pages/Login';
+
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
+// Import apollo from @apollo/client
 
-export default App;
+// set token context
+// import { setContext } from "@apollo/client/link/context";
+
+// establish new link to GraphQL
+
+// define authLink
+
+// define client and initiate a new cashe object using new InMemoryCache()
+
+export default function App() {
+    return (
+        // wrap all in ApolloProvider
+        <Router>
+            <div>
+                <Header />
+                <div>
+                    <Routes>
+                        {/* <Route path="/" element={<Login />} /> */}
+                        <Route path="/home" element={<Home />} />
+                        {/* <Route path="/login" element={<Login />} /> */}
+                        {/* <Route path="/signup" element={<Signup />} /> */}
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </Router>
+    );
+};
