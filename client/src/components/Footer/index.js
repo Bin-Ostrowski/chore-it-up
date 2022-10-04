9cfdb012b3d9fd3f618950341185aed4281a693c
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-10-04 14:09:54 -0700

    testing chakra styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Box } from '@chakra-ui/react';
 
 const Footer = () => {
     return (
         <footer>
-            <div>&copy;2022 by Chore It Up</div>
+            <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                w="100%"
+                h="50px"
+                bgColor="green.400"
+                bottom="0px"
+            >
+                <div>&copy;2022 by Chore It Up</div>
+            </Box>
         </footer>
     );
 };
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,26 +1,56 @@
 import React from 'react';
 // import Auth from "../../utils/auth";
 import { Link } from 'react-router-dom';
+import { Box } from '@chakra-ui/react';
 
 const Header = () => {
     // define logout
 
+    const navBoxStyles = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'left',
+        padding: '20px',
+        textAlign: 'center',
+        color: 'black',
+        fontWeight: 'bold'
+    };
+
+    const hoverStyles = {
+        textShadow: '0 0 20px black',
+    };
+
     return (
         <header>
-            <div>
-                <Link to="/">
-                    <h1>Chore It Up</h1>
-                </Link>
+            <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                w="100%"
+                h="50px"
+                bgColor="green.400"
+            >
+                <Box sx={navBoxStyles} _hover={hoverStyles}>
+                    <Link to="/">
+                        <h1>Chore It Up</h1>
+                    </Link>
+                </Box>
                 <nav>
                     {/* add Auth.login() ? () */}
-                    <>
-                        <Link to="/home">Home</Link>
-                        <Link to="/chores">Your Chores</Link>
-                        {/* <Link to="/login">Login</Link>
+                    <Box sx={navBoxStyles}>
+                        <>
+                            <Box _hover={hoverStyles}>
+                                <Link to="/home">Home</Link>
+                            </Box>
+                            <Box paddingLeft='20px' _hover={hoverStyles}>
+                                <Link to="/chores">Your Chores</Link>
+                            </Box>
+                            {/* <Link to="/login">Login</Link>
                         <Link to="/signup">Signup</Link> */}
-                    </>
+                        </>
+                    </Box>
                 </nav>
-            </div>
+            </Box>
         </header>
     );
 };
