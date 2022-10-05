19285fa752be64e78301151d85f4777cb08cec4b
lorreina <slorreina369@gmail.com>
2022-10-05 15:44:33 -0700

    created type User and type User and some of type Mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,7 +12,35 @@ const typeDefs = gql`
         dueDate: String
     }
 
+    type Group {
+        _id: ID
+        groupName: String
+        users: [User]
+        chores: [Chore]
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        groups: [Group]
+        chores: [Chore]
+    }
+
     type Query {
+        me: User
+        users: [User]
+        user(username: String!): Thought
+        groups(groupName: String!): [Group]
+        group(_id: ID!): Group
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addGroup(groupName: String!): Group
+        removeGroup(_id: ID): Group
     }
 `;
 
