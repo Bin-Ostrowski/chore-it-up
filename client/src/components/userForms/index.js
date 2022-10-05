77179c53a0501c75c36a438ca13e98e01963e169
bg <62928419+bgeertsen@users.noreply.github.com>
2022-10-04 21:02:57 -0700

    styled login page for desktop view

---
+++ client/src/components/userForms/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+
+const UserForms = () => {
+    return (
+        <div className="login-container">
+            <div className="login-form">
+                <div className="form-header">
+                    <h2>Login</h2>
+                </div>
+                <FormControl>
+                    <FormLabel>Username</FormLabel>
+                    <Input type="username" />
+                    <FormLabel>Password</FormLabel>
+                    <Input type="password" />
+                    <Button
+                        className="button"
+                        colorScheme="green.400"
+                        variant="outline"
+                        size="lg"
+                    >
+                        Login
+                    </Button>
+                </FormControl>
+            </div>
+            <div className="login-form">
+                <div className="form-header">
+                    <h2>Sign Up</h2>
+                </div>
+                <FormControl>
+                    <FormLabel>Username</FormLabel>
+                    <Input type="username" />
+                    <FormLabel>Email address</FormLabel>
+                    <Input type="email" />
+                    <FormLabel>Password</FormLabel>
+                    <Input type="password" />
+                    <Button
+                        className="button"
+                        colorScheme="green.400"
+                        variant="outline"
+                        size="lg"
+                    >
+                        Sign Up
+                    </Button>
+                </FormControl>
+            </div>
+        </div>
+    );
+};
+
+export default UserForms;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import UserForms from '../components/userForms';
+
+const Login = () => {
+    return <UserForms />;
+};
+
+export default Login;
