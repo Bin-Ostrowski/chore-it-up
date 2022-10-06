e687a04e630ddf0439b1fe7c148ff6e36ed0a8e7
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-10-05 20:18:22 -0700

    add choreForm submit handeling

--- client/src/components/ChoreForm/index.js
+++ client/src/components/ChoreForm/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import {
     FormControl,
     FormLabel,
@@ -12,6 +13,47 @@ import {
 import './choreForm.css';
 
 const ChoreForm = () => {
+    // set State for inputs
+    const [choreName, setChoreName] = useState('');
+    const [dueDate, setDueDate] = useState('');
+    const [assignedTo, setAssignedTo] = useState('');
+    const [choreBody, setChoreBody] = useState('');
+
+    // declare addChore() and error variable for mutation
+
+    // input onChange hangler
+    const handleChange = (event) => {
+        switch (event.target.name) {
+            case 'choreName':
+                setChoreName(event.target.value)
+                break;
+            case 'dueDate':
+                setDueDate(event.target.value)
+                break;
+            case 'assignedTo':
+                setAssignedTo(event.target.value)
+                break;
+            case 'choreBody':
+                setChoreBody(event.target.value)
+                break;
+        }
+    };
+
+    // form submit handler -
+    const handleFormSubmit = async (event) => {
+        console.log('clicked');
+        event.preventDefault();
+
+        console.log(choreName, dueDate, assignedTo, choreBody);
+
+        // addChore mutation
+
+    //     setChoreName('');
+    //     setDueDate('');
+    //     setAssignedTo('');
+    //     setChoreBody('');
+    };
+
     return (
         <div className="form-container">
             <FormControl isRequired>
@@ -19,7 +61,12 @@ const ChoreForm = () => {
                     <div className="input-container">
                         <div className="form-input">
                             <FormLabel>Chore Name:</FormLabel>
-                            <Input placeholder="Chore Name" />
+                            <Input
+                                placeholder="Chore Name"
+                                value={choreName}
+                                name='choreName'
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className="form-input">
                             <FormLabel>Finish By Date:</FormLabel>
@@ -27,13 +74,21 @@ const ChoreForm = () => {
                                 placeholder="Select Date"
                                 size="md"
                                 type="datetime-local"
+                                value={dueDate}
+                                name='dueDate'
+                                onChange={handleChange}
                             />
                         </div>
                         <div className="form-input">
                             <FormLabel optionalIndicator>
                                 Assigned To:
                             </FormLabel>
-                            <Select placeholder="Select Username">
+                            <Select
+                                placeholder="Select Username"
+                                value={assignedTo}
+                                name='assignedTo'
+                                onChange={handleChange}
+                            >
                                 {/* Map over users in group */}
                                 <option>Luffy</option>
                                 <option>Nami</option>
@@ -46,11 +101,19 @@ const ChoreForm = () => {
                             <FormLabel optionalIndicator>
                                 Chore Notes:
                             </FormLabel>
-                            <Input placeholder="Describe Chore" />
+                            <Input
+                                placeholder="Describe Chore"
+                                value={choreBody}
+                                name='choreBody'
+                                onChange={handleChange}
+                            />
                         </div>
                     </div>
                     <div className="form-btn">
-                        <Button colorScheme="blue">Add This Chore</Button>
+                        <Button colorScheme="blue" type="click"
+                        onClick={handleFormSubmit}>
+                            Add This Chore
+                        </Button>
                     </div>
                 </div>
             </FormControl>
