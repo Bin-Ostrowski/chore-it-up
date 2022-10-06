b36047ba516ef57acd1c78dd46bb562a7f2c81ae
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-10-06 15:05:00 -0700

    refactored UpdateChoreModal to be a seperate component

--- client/src/components/ChoreList/index.js
+++ client/src/components/ChoreList/index.js
@@ -1,30 +1,20 @@
 import React from 'react';
-import ChoreForm from '../ChoreForm';
+import UpdateChoreModal from '../UpdateChoreModal';
 import './choreList.css';
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    useDisclosure
-} from '@chakra-ui/react';
-
 
 // do we ant to link the group to view SingleGroup page?
 // import { Link } from "react-router-dom";
 
 // pass in props for chores array, group array for that user
 const ChoreList = () => {
-
-    // set useDisclosure for modal
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
     // conditionaly render chores
-    
+
+    // remove Chore
+    const removeChore = (chore) => {
+        // add mutation to updatechore to running tally.
+        console.log('chore removed');
+    };
+
     // else return map of list of thoughts
     return (
         <ul>
@@ -37,33 +27,10 @@ const ChoreList = () => {
                     </div>
                 </div>
                 <div className="list-btns">
-                    <button onClick={onOpen} className="btn">update chore</button>
-                    
-                    <Modal isOpen={isOpen} onClose={onClose}>
-                        <ModalOverlay />
-                        <ModalContent>
-                            <ModalHeader>Update Chore</ModalHeader>
-                            <ModalCloseButton />
-                            <ModalBody>
-                                <ChoreForm />
-                            </ModalBody>
-
-                            <ModalFooter>
-                                <Button
-                                    colorScheme="blue"
-                                    mr={3}
-                                    onClick={onClose}
-                                >
-                                    Close
-                                </Button>
-                                <Button variant="ghost"  onClick={onClose}>
-                                    Save Updated Chore
-                                </Button>
-                            </ModalFooter>
-                        </ModalContent>
-                    </Modal>
-
-                    <button className="btn">finish chore</button>
+                    <UpdateChoreModal />
+                    <button className="btn" onClick={removeChore}>
+                        finish chore
+                    </button>
                     <button className="btn">add to google calendar</button>
                 </div>
             </li>
--- client/src/components/UpdateChoreModal/index.js
+++ client/src/components/UpdateChoreModal/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
     Modal,
     ModalOverlay,
@@ -7,43 +8,141 @@ import {
     ModalBody,
     ModalCloseButton,
     Button,
+    useDisclosure,
     FormControl,
     FormLabel,
-    Input
+    Input,
+    Select,
 } from '@chakra-ui/react';
 
-import React from 'react';
-import ChoreForm from '../ChoreForm';
-const { isOpen, onOpen, onClose } = useDisclosure();
+const UpdateChoreModal = () => {
+    // set useDisclosure for updateChore modal
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-<Modal isOpen={isOpen} onClose={onClose}>
-    <ModalOverlay />
-    <ModalContent>
-        <ModalHeader>Update Chore</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-            <FormControl isRequired>
-                <div className="flex-row">
-                    <div className="input-container">
-                        <div className="form-input">
-                            <FormLabel>Chore Name:</FormLabel>
-                            <Input
-                                placeholder="Chore Name"
-                                value={choreName}
-                                name="choreName"
-                                onChange={handleChange}
-                            />
+    // set State for inputs
+    const [choreName, setChoreName] = useState('');
+    const [dueDate, setDueDate] = useState('');
+    const [assignedTo, setAssignedTo] = useState('');
+    const [choreBody, setChoreBody] = useState('');
+
+    // input onChange hangler
+    const handleChange = (event) => {
+        switch (event.target.name) {
+            case 'choreName':
+                setChoreName(event.target.value);
+                break;
+            case 'dueDate':
+                setDueDate(event.target.value);
+                break;
+            case 'assignedTo':
+                setAssignedTo(event.target.value);
+                break;
+            case 'choreBody':
+                setChoreBody(event.target.value);
+                break;
+        }
+    };
+
+    // form submit handler -
+    const handleFormSubmit = async (event) => {
+        console.log('clicked');
+        event.preventDefault();
+
+        console.log(choreName, dueDate, assignedTo, choreBody);
+
+        // update mutation
+
+        //     setChoreName('');
+        //     setDueDate('');
+        //     setAssignedTo('');
+        //     setChoreBody('');
+    };
+
+    return (
+        <div>
+            <button onClick={onOpen} className="btn">
+                update chore
+            </button>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Update Chore</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <div className="form-container">
+                            <FormControl isRequired>
+                                <div className="input-container">
+                                    <div className="form-input">
+                                        <FormLabel>Chore Name:</FormLabel>
+                                        <Input
+                                            placeholder="Chore Name"
+                                            value={choreName}
+                                            name="choreName"
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="form-input">
+                                        <FormLabel>Finish By Date:</FormLabel>
+                                        <Input
+                                            placeholder="Select Date"
+                                            size="md"
+                                            type="datetime-local"
+                                            value={dueDate}
+                                            name="dueDate"
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="form-input">
+                                        <FormLabel optionalIndicator>
+                                            Assigned To:
+                                        </FormLabel>
+                                        <Select
+                                            placeholder="Select Username"
+                                            value={assignedTo}
+                                            name="assignedTo"
+                                            onChange={handleChange}
+                                        >
+                                            {/* Map over users in group */}
+                                            <option>Luffy</option>
+                                            <option>Nami</option>
+                                            <option>Chopper</option>
+                                            <option>Zoro</option>
+                                            <option>None</option>
+                                        </Select>
+                                    </div>
+                                    <div className="form-input">
+                                        <FormLabel optionalIndicator>
+                                            Chore Notes:
+                                        </FormLabel>
+                                        <Input
+                                            placeholder="Describe Chore"
+                                            value={choreBody}
+                                            name="choreBody"
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+                            </FormControl>
                         </div>
-                    </div>
-                </div>
-            </FormControl>
-        </ModalBody>
+                    </ModalBody>
 
-        <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-                Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
-        </ModalFooter>
-    </ModalContent>
-</Modal>;
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button
+                            variant="ghost"
+                            type="click"
+                            onClick={handleFormSubmit}
+                        >
+                            Save Updated Chore
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </div>
+    );
+};
+
+export default UpdateChoreModal;
