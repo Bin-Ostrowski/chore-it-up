53616703095e35028a6ad8851b8b2d9d43fea72f
lorreina <slorreina369@gmail.com>
2022-10-06 10:21:51 -0700

    change name for localhost in connections

--- server/config/connections.js
+++ server/config/connections.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/google-books',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        //useCreateIndex: true,
-        // useFindAndModify: false,
-    }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+    //commented out due to code potentionally obsolete
+    //useCreateIndex: true,
+    //useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
