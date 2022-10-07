80a841137612e9c380bcceef0dc7807ad34887b2
bg <62928419+bgeertsen@users.noreply.github.com>
2022-10-06 22:32:47 -0700

    added initial group form

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,6 +3,17 @@ import React from 'react';
 // import GroupList from "../components/GroupList";
 import ChoreForm from '../../components/ChoreForm';
 import ChoreList from '../../components/ChoreList';
+
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    Button,
+    ButtonGroup,
+} from '@chakra-ui/react';
+
 import './home.css';
 
 const Home = () => {
@@ -13,6 +24,19 @@ const Home = () => {
     return (
         <main>
             <h2>Group Form</h2>
+            <div className="add-group-container">
+                <FormControl className="add-group-form">
+                    <Button colorScheme="green" variant="solid" size="lg">
+                        Add new group!
+                    </Button>
+                    <FormLabel>Group Name:</FormLabel>
+                    <Input
+                        className="group-input"
+                        width="200px"
+                        style={{ border: '1px solid black' }}
+                    />
+                </FormControl>
+            </div>
             <div className="groups-display">
                 {/* //map over group id */}
                 <div className="group-container">
