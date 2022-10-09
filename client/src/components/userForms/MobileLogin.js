1f030ead9a9ac27f3bf1acbbf658003499e851ff
bg <62928419+bgeertsen@users.noreply.github.com>
2022-10-09 11:31:10 -0700

    mobile login functionality is now in place

--- client/src/components/userForms/MobileLogin.js
+++ client/src/components/userForms/MobileLogin.js
@@ -11,9 +11,53 @@ import {
     Button,
 } from '@chakra-ui/react';
 
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
+import Auth from '../../utils/auth';
+
 const MobileLogin = () => {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     const [isLoginHidden, setIsLoginHidden] = useState(true);
     const [isSignUpHidden, setIsSignUpHidden] = useState(true);
+    const [loginFormState, setLoginFormState] = useState({
+        email: '',
+        password: '',
+    });
+
+    const [login, { loginError }] = useMutation(LOGIN_USER);
+    const [isError, setIsError] = useState();
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setLoginFormState({
+            ...loginFormState,
+            [name]: value,
+        });
+    };
+
+    const handleLoginFormSubmit = async (event) => {
+        event.preventDefault();
+        if (!loginFormState.email.match(emailRegex)) {
+            setIsError(true);
+        } else if (loginFormState.password.length < 5) {
+            setIsError(true);
+        } else {
+            setIsError(false);
+        }
+
+        if (!isError) {
+            try {
+                const { data } = await login({
+                    variables: { ...loginFormState },
+                });
+
+                Auth.login(data.login.token);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    };
 
     function toggleLogin() {
         if (!isSignUpHidden) {
@@ -43,12 +87,29 @@ const MobileLogin = () => {
                     >
                         <h2>Login</h2>
                     </div>
-                    <FormControl>
-                        <FormLabel fontSize="2xl">Username</FormLabel>
-                        <Input type="username" />
+                    <FormControl isInvalid={isError} isRequired>
+                        <FormLabel fontSize="2xl">Email</FormLabel>
+                        <Input
+                            name="email"
+                            id="email"
+                            onChange={handleChange}
+                            type="email"
+                        />
                         <FormLabel fontSize="2xl">Password</FormLabel>
-                        <Input type="password" />
+                        <Input
+                            name="password"
+                            id="password"
+                            onChange={handleChange}
+                            type="password"
+                        />
+                        {isError && (
+                            <FormErrorMessage className="error">
+                                invalid login info!
+                            </FormErrorMessage>
+                        )}
+
                         <Button
+                            onClick={handleLoginFormSubmit}
                             className="button"
                             colorScheme="green.400"
                             variant="solid"
