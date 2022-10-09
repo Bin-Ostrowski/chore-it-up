22e0670206e987466188e9ee1b86c638e7fb5267
bg <62928419+bgeertsen@users.noreply.github.com>
2022-10-08 18:21:16 -0700

    added conditional rednering of group form and populates the group list with the user's group if it exists

---
+++ client/src/utils/queries.js
@@ -0,0 +1,41 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            group {
+                _id
+                groupName
+            }
+        }
+    }
+`;
+
+export const QUERY_ME = gql`
+    {
+        me {
+            _id
+            username
+            group {
+                _id
+                groupName
+                users {
+                    _id
+                    username
+                }
+                chores {
+                    _id
+                    choreName
+                    choreBody
+                    createdAt
+                    username
+                    assignedTo
+                    dueDate
+                    status
+                }
+            }
+        }
+    }
+`;
