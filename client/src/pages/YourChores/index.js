f41d10c77abd62d776e6498bcf96fb42d71133f0
bg <62928419+bgeertsen@users.noreply.github.com>
2022-10-11 17:28:55 -0700

    started fixing yourChores

--- client/src/pages/YourChores/index.js
+++ client/src/pages/YourChores/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 import ChoreList from '../../components/ChoreList';
 import auth from '../../utils/auth';
-
+import { QUERY_ME } from '../../utils/queries';
 import './youChores.css';
 // import ChoreForm from '../components/ChoreForm';
 
@@ -19,7 +20,13 @@ const YourChores = () => {
     // define and deconstruct userID
 
     // deconstruct variables loading and data from userQuery Hook for Query_me?
+    const { loading, error, data } = useQuery(QUERY_ME);
 
+    console.log(data);
+
+    if (!data.me.chores.length) {
+        return <h2>You don't currently have any chores! Nice Job!</h2>;
+    }
     // loading variable used to briefly show a loading <div> element
     return (
         <div className="your-chores-container">
@@ -27,10 +34,10 @@ const YourChores = () => {
                 <h2 className="title">UserName's Chores</h2>
             </div>
             <div className="list-container">
-                <ChoreList
+                {/* <ChoreList
                 // pass groups={groups}
                 // chores={chores}
-                />
+                /> */}
             </div>
         </div>
     );
