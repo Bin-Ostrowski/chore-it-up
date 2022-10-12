530012715f0e6a5b64341c51bc3accc34a602f36
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-10-11 22:00:49 -0700

    wrapped choreForm in <form>

--- client/src/components/ChoreForm/index.js
+++ client/src/components/ChoreForm/index.js
@@ -28,18 +28,17 @@ const ChoreForm = ({ refetch, groupData }) => {
     const userId = data.me._id;
     console.log(userId);
     // set State for inputs
-    const [choreData, setChoreData] = useState({
-        // group: groupId,
-        // userId: userId,
-        // choreName: 'testName',
-        // dueDate: '2022-10-11T23:11',
-        // // assignedTo: 'Nami',
-        // choreBody: 'testBody',
-    });
+    const [choreData, setChoreData] = useState({});
     console.log('choreForm', choreData);
 
     // Set error State
     const [isError, setIsError] = useState(false);
+
+    // error message state
+    const [errorMessage, setErrorMessage] = useState('');
+
+    // set button error
+    const [buttonError, setbuttonError] = useState(true);
 
     // declare addChore() and error variable for mutation
     const [addChore, { error }] = useMutation(ADD_CHORE);
@@ -55,15 +54,36 @@ const ChoreForm = ({ refetch, groupData }) => {
 
             [name]: value,
         });
+
+        // if (choreData.choreName === '') {
+        //     setErrorMessage('Chore Name is required!');
+        //     setIsError(true);
+        //     setbuttonError(true);
+        // } else if (choreData.assignedTo === '') {
+        //     setErrorMessage('Please assign chore to a member!');
+        //     setIsError(true);
+        //     setbuttonError(true);
+        // } else {
+        //     setIsError(false);
+        setbuttonError(false);
+        // }
     };
 
     // form submit handler -
     const handleFormSubmit = async (event) => {
         console.log('handleFormSubmit');
         event.preventDefault();
+
         if (choreData.choreName === '') {
+            setErrorMessage('Chore Name is required!');
             setIsError(true);
+            setbuttonError(true);
+        } else if (choreData.assignedTo === '') {
+            setErrorMessage('Please assign chore to a member!');
+            setIsError(true);
+            setbuttonError(true);
         } else {
+            setbuttonError(false);
             setIsError(false);
             // setChoreData({ ...choreData });
             console.log({ ...choreData });
@@ -96,86 +116,99 @@ const ChoreForm = ({ refetch, groupData }) => {
     }
     return (
         <div className="form-container">
-            <FormControl className="flex-row" isInvalid={isError} isRequired>
-                <div className="input-container">
-                    <div className="form-input">
-                        <FormLabel className="form-lable">
-                            Chore Name:
-                        </FormLabel>
-                        <Input
-                            focusBorderColor="lime"
-                            placeholder="Chore Name"
-                            value={choreData.choreName}
-                            variant="filled"
-                            name="choreName"
-                            size="sm"
-                            onChange={handleChange}
-                        />
-                        {isError && (
-                            <FormErrorMessage className="error">
-                                Chore name is required.
-                            </FormErrorMessage>
-                        )}
+            <form onSubmit={handleFormSubmit}>
+                <FormControl
+                    className="flex-row"
+                    isInvalid={isError}
+                    isRequired
+                >
+                    <div className="input-container">
+                        <div className="form-input">
+                            <FormLabel className="form-lable">
+                                Chore Name:
+                            </FormLabel>
+                            <Input
+                                required
+                                focusBorderColor="lime"
+                                placeholder="Chore Name"
+                                value={choreData.choreName}
+                                variant="filled"
+                                name="choreName"
+                                size="sm"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-input">
+                            <FormLabel requiredIndicator>
+                                Finish By Date:
+                            </FormLabel>
+                            <Input
+                                focusBorderColor="lime"
+                                placeholder="Select Date"
+                                size="sm"
+                                type="datetime-local"
+                                variant="filled"
+                                value={choreData.dueDate}
+                                name="dueDate"
+                                onChange={handleChange}
+                                isInvalid
+                                errorBorderColor="null"
+                            />
+                        </div>
+                        <div className="form-input">
+                            <FormLabel>Assigned To:</FormLabel>
+                            <Select
+                                required
+                                focusBorderColor="lime"
+                                placeholder="Select Username"
+                                value={choreData.assignedTo}
+                                name="assignedTo"
+                                variant="filled"
+                                size="sm"
+                                onChange={handleChange}
+                            >
+                                {/* Map over users in group */}
+                                {groupData.group.users.map((user, i) => (
+                                    <option key={user._id}>
+                                        {user.username}
+                                    </option>
+                                ))}
+                            </Select>
+                        </div>
+                        <div className="form-input">
+                            <FormLabel requiredIndicator>
+                                Chore Notes:
+                            </FormLabel>
+                            <Input
+                                focusBorderColor="lime"
+                                placeholder="Describe Chore"
+                                value={choreData.choreBody}
+                                name="choreBody"
+                                variant="filled"
+                                size="sm"
+                                onChange={handleChange}
+                                isInvalid
+                                errorBorderColor="null"
+                            />
+                        </div>
                     </div>
-                    <div className="form-input">
-                        <FormLabel requiredIndicator>Finish By Date:</FormLabel>
-                        <Input
-                            focusBorderColor="lime"
-                            placeholder="Select Date"
-                            size="sm"
-                            type="datetime-local"
-                            variant="filled"
-                            value={choreData.dueDate}
-                            name="dueDate"
-                            onChange={handleChange}
-                            isInvalid
-                            errorBorderColor="null"
-                        />
-                    </div>
-                    <div className="form-input">
-                        <FormLabel requiredIndicator>Assigned To:</FormLabel>
-                        <Select
-                            focusBorderColor="lime"
-                            placeholder="Select Username"
-                            value={choreData.assignedTo}
-                            name="assignedTo"
-                            variant="filled"
-                            size="sm"
-                            onChange={handleChange}
-                            isInvalid
-                            errorBorderColor="null"
+                    <div className="form-btn">
+                        <Button
+                            colorScheme="green"
+                            type="click"
+                            disabled={buttonError}
+                            // onClick={handleFormSubmit}
                         >
-                            {/* Map over users in group */}
-                            {groupData.group.users.map((user, i) => (
-                                <option key={user._id}>{user.username}</option>
-                            ))}
-                        </Select>
+                            Add This Chore
+                        </Button>
                     </div>
-                    <div className="form-input">
-                        <FormLabel requiredIndicator>Chore Notes:</FormLabel>
-                        <Input
-                            focusBorderColor="lime"
-                            placeholder="Describe Chore"
-                            value={choreData.choreBody}
-                            name="choreBody"
-                            variant="filled"
-                            size="sm"
-                            onChange={handleChange}
-                            isInvalid
-                            errorBorderColor="null"
-                        />
-                    </div>
-                </div>
-                <div className="form-btn">
-                    <Button
-                        colorScheme="green"
-                        type="click"
-                        onClick={handleFormSubmit}
-                    >
-                        Add This Chore
-                    </Button>
-                </div>
-            </FormControl>
+                    {isError && (
+                        <FormErrorMessage className="error">
+                            {errorMessage}
+                        </FormErrorMessage>
+                    )}
+                </FormControl>
+            </form>
         </div>
     );
 };
