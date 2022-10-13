4035192765c8633f8ba0d6da075d5b56394f0472
bg <62928419+bgeertsen@users.noreply.github.com>
2022-10-12 17:22:45 -0700

    added process.env for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ const isLoggedin = auth.loggedIn();
 
 // establish new link to GraphQL
 const httpLink = createHttpLink({
-    uri: '/graphql',
+    uri: process.env.REACT_APP_URI || 'http://localhost:3001/graphql',
 });
 
 // define authLink
